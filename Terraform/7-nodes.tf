c38a02f3098b1dd16bd620a5074ded7cf73af91b
Deepak verma <deepcloud.28@gmail.com>
2023-11-06 15:20:52 +0530

    Terraform/7-nodes.tf

--- Terraform/7-nodes.tf
+++ Terraform/7-nodes.tf
@@ -50,7 +50,7 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t2.medium"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 3
     max_size     = 10
     min_size     = 0
   }
